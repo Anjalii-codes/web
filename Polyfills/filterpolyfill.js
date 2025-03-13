66da27589d53fe513abbe5cbf8a80cdc3008a197
Anjali Prasad <1941012995.b.anjaliprasad@gmail>
2025-03-13 13:06:18 +0530

    modifying polyfill

--- Polyfills/filterpolyfill.js
+++ Polyfills/filterpolyfill.js
@@ -1,6 +1,19 @@
+// return a new array
+Array.prototype.myfilter=function(userfunc){
+    let contextt=this
+    newarray=[]
+    for(let i=0;i<contextt.length;i++){
+        if(userfunc(contextt[i],i,contextt)){
+            newarray.push(contextt[i])
+
+        }
+
+    }
+    return newarray
+}
 let a=[1,2,30,5]
-let b= a.filter((e)=>{
-    if(n%2==0){
+let b= a.myfilter((e)=>{
+    if(e%2==0){
         return e
     }
 })
